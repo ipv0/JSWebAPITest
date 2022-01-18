3867a4d2634042186706c3a63bb55327b64357e9
Alex A <lxcode@gmail.com>
2022-01-17 21:37:15 -0500

    first test commit

---
+++ scripts.js
@@ -0,0 +1,216 @@
+/* eslint-disable no-unused-vars */
+// const p = function(text) {
+//     console.log(text);
+// }
+
+let p = (text) => { 
+    console.log(text);
+}
+
+
+/* --- JQuery animations and stuff --- */
+
+$(document).ready(function() {
+    console.log('jQuery: document ready event')
+    $('#navbar').hide().fadeIn(300);
+
+    $('#navbar a').click(function() {
+        $(this).fadeOut(60).fadeIn(40);
+    })
+
+    assignEventListeners();
+
+});
+
+$(document).ready(function () {
+    
+});
+
+/* --- Event listeners assignment --- */
+let assignEventListeners = function () {
+    p('assignEventListeners func called');
+
+    const link2Button = document.querySelector('#link-2');
+    const apiButton   = document.querySelector('#link-3');
+    const link4Button = document.querySelector('#link-4');
+    const link5Button = document.querySelector('#link-5');
+    const loadWeatherButton = document.querySelector('#loadWeatherButton');
+
+    apiButton.addEventListener('click', function () {
+        apiButtonclick();
+    });
+
+    // when assigning event listener callbacks like that, remember
+    // not to put brackets after the  funcktion name, like below
+    link2Button.addEventListener('click', link2Click);
+
+    link4Button.addEventListener('click', link4Click);
+
+    link5Button.addEventListener('click', link5Click);
+
+
+    if (loadWeatherButton)
+        loadWeatherButton.addEventListener("click", testButtonClick);
+
+}
+
+function testButtonClick () {
+    p('testButton Click');
+    getWeatherData();
+    //$("#dialog").dialog();
+}
+
+function link2Click() {
+    getWeatherData();
+
+    let sampleText = `
+    <br />
+    <button id = 'loadWeatherButton' class = 'button'>
+    Load Weather Data
+    </button>
+    <p id = "stuffText">  </p>
+    `;
+    createBox("Weather", sampleText);
+
+    // creates the container for data boxes, then adds individual boxes 
+    // createDataBox(Box CSS Id, box FadeIn Time, Box Content)
+    createDataBoxesDiv(function() {
+        createDataBox('temperatureBox', 400, 'Temp.');
+        createDataBox('feelsLikeBox', 600, 'Feels File');
+        createDataBox('tempMinBox', 800, 'Temp. Min.');
+        createDataBox('humidityBox', 1000, 'Humidity');
+        createDataBox('humidityBox2', 1200, 'Test1 <br /> data');
+        createDataBox('humidityBox3', 1400, 'Test2 <br /> data');
+        createDataBox('humidityBox4', 1600, 'Test3 <br /> data');
+    });
+
+    assignEventListeners();
+
+}
+
+/* 
+    PASS THE INDIVIDUAL DATA BOX CREATION FUNCTIONS INTO THE
+    CREATEDATABOXESDIV() FUNCTION BELOW.
+*/
+function createDataBoxesDiv(func) {
+    let boxContent = document.querySelector('#boxContent');
+    let dataBoxesDiv = document.createElement('div');
+   
+    dataBoxesDiv.id = 'dataBoxesDiv';
+
+    boxContent.append(dataBoxesDiv);
+
+    func();
+}
+
+
+// --- creates little boxes with given parameters and data ---
+function createDataBox(boxId, boxFadeTime, boxContentHTML) {
+
+    // the container box created by cerateDataBoxesDiv(), where the little boxes will be added. 
+    let dataBoxesDiv = document.querySelector('#dataBoxesDiv');
+
+    let dataBox = document.createElement('div');
+    dataBox.id = boxId;
+    dataBox.classList.add('dataBox');
+    dataBox.innerHTML = boxContentHTML;
+
+    dataBoxesDiv.append(dataBox);
+
+    $("#" + boxId).hide();
+    $("#" + boxId).fadeIn(boxFadeTime);
+}
+
+/* ------------------------------------------- */
+function apiButtonclick() {
+    p('API Button clicked');
+    /* write the rest here... */
+
+    createBox("API", "<p>Sample box content text.</p>");
+
+    createDataBoxesDiv(function() {
+        createDataBox('apiBox1', 1200, 'Test1 <br /> data');
+        createDataBox('apiBox2', 1400, 'Test2 <br /> data');
+        createDataBox('apiBox3', 1600, 'Test3 <br /> data');
+    });
+
+}
+/* -------------------------------------------- */
+
+function link4Click() {
+    p('link4 click');
+
+    createBox("Other", "Sample Other Text.");
+
+    createDataBox('asd', 600, 'Feels File');
+    
+}
+
+function link5Click() {
+    createBox("Google", "<a href='#'> Google </a>");
+}
+
+
+let createBox = function(boxTitle, BoxContentHTML) {
+
+    $('#box').remove();
+
+    /* creating the box element where my content will be displayed */
+    let box = document.createElement('div'); //creating the actual element
+    box.classList.add('box-class'); //adding class
+    box.id = 'box'; // adding an id
+    box.style.display = 'none'; //making it hidden, so it can later be shown via jQuery
+    
+    /* creating box title */
+    let boxTitleElement = document.createElement('p');
+    boxTitleElement.classList.add('box-title-class');
+    boxTitleElement.innerText = boxTitle;
+    
+    document.getElementById('content').append(box); //add the box to the DOM
+    box.append(boxTitleElement);//add the box title element to the box
+
+    /* the box that contains the actual thing we need to display */
+    let boxContent = document.createElement('div');
+    boxContent.id = "boxContent";
+    boxContent.classList.add('box-content');
+    boxContent.innerHTML = BoxContentHTML;
+    box.append(boxContent);
+
+    $('#box').fadeIn(200);
+
+}
+
+
+// Converts temperature from K to C
+function convertTemperature(kelvins) {
+    return (kelvins - 273).toFixed(2);
+}
+
+function getWeatherData() {
+    let temp = 0;
+    let apik = "3045dd712ffe6e702e3245525ac7fa38"
+    let query = 'https://api.openweathermap.org/data/2.5/weather?q=Toronto'+'&appid='+apik;
+
+    fetch(query)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+
+        temp = convertTemperature(data.main.temp);
+        document.querySelector('#temperatureBox').innerHTML = 
+        "Temp. <br />" + temp + " &#176;C";
+
+        temp = convertTemperature(data.main.temp_min);
+        document.querySelector('#tempMinBox').innerHTML = 
+        "Temp. Min.<br />" + temp + " &#176;C";
+
+        document.querySelector('#humidityBox').innerHTML = 
+        "Humidity <br />" + data.main.humidity + "%";
+
+        temp = convertTemperature(data.main.feels_like);
+        document.querySelector('#feelsLikeBox').innerHTML = 
+        "Feels Like <br />" + temp + " &#176;C";
+        
+    });
+}
+
